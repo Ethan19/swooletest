80dfc0f4c300aa4c3b5dd33367a4f68631905124
Ethan19 <flymazda@163.com>
2016-06-16 19:10:13 +0800

    add redis table

--- http_websocket.php
+++ http_websocket.php
@@ -8,15 +8,19 @@ mysql部分
 /********************
 服务
 ********************/
-$server = new swoole_websocket_server("0.0.0.0", 9502);
+$redis = new swoole_redis;
+
+$server = new swoole_websocket_server("0.0.0.0", 9501);
 
 $server->on('open', function (swoole_websocket_server $server, $request) {//建立链接
-    //var_dump($server);
+    
+    // $type = "login";
+    // $msg = $request->fd."已登录";
+    // $reply = json_decode(array("type"=>$type,"msg"=>$msg));
+    // $server->push($request->fd,$reply);
 });
 
-$server->on('message', function (swoole_websocket_server $server, $frame) {//接受消息
-    $
-
+$server->on('message', function (swoole_websocket_server $server, $frame){//接受消息
     if($fd){
         $fd = ($fd>=$frame->fd)?$fd:$frame->fd;
     }else{
@@ -24,18 +28,62 @@ $server->on('message', function (swoole_websocket_server $server, $frame) {//接
     }
     $data = json_decode($frame->data);
 
-    for ($i=1; $i <=$fd ; $i++) { 
-        $res = 'data is'.$data  . 'fd=' . $fd."\n";
-        echo $res;
-        $server->push($i,$res);
+    if($data->type == "login"){
+        $replyTtype = "login";
+        $msg = '用户'.$fd."登录\n";
+        $res = array("replyTtype"=>$replyTtype,"msg"=>$msg); 
+        $jsonRes = json_encode($res);
+        addUser($frame->fd);
+    }elseif($data->type == "message"){
+
     }
 
-
+    //var_dump($data->type);
+    for ($i=1; $i <=$fd ; $i++) {
+        $server->push($i,$jsonRes);
+    }
 });
 
 //$server->send();
-$server->on('close', function ($ser, $fd) {
+$server->on('close', function ($ser, $fd){
+    delUser($fd);
     echo "client {$fd} closed\n";
 });
-
 $server->start();
+
+/**
+ * [addUser 往redis增加用户]
+ * @author 1023
+ * @date    2016-06-16
+ */
+function addUser($fd){
+    global $redis;
+        $redis->connect('127.0.0.1', 6379, function (swoole_redis $redis, $result) use($fd){
+            $redis->set("user-".$fd,"fd-".$fd, function($redis,$result){ 
+                var_dump($result);
+            });
+        });
+}
+/**
+ * [delUser 删除redis某个用户]
+ * @author 1023
+ * @date          2016-06-16
+ * @return [type] [description]
+ */
+function delUser($fd){
+    global $redis;
+        $redis->connect('127.0.0.1', 6379, function (swoole_redis $redis, $result) use($fd){
+            $redis->del("user-".$fd, function($redis,$result){ 
+                var_dump($result);
+            });
+        });
+}
+/**
+ * [getListUser 获取redis用户列表]
+ * @author 1023
+ * @date          2016-06-16
+ * @return [type] [description]
+ */
+function getListUser(){
+
+}

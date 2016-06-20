121c02151fa8ddd35331664b9cdb3751e2b80894
Ethan19 <flymazda@163.com>
2016-06-20 19:02:05 +0800

    talk room

--- http_websocket.php
+++ http_websocket.php
@@ -10,43 +10,56 @@ mysql部分
 ********************/
 $redis = new swoole_redis;
 
+// var_dump($redis);
 $server = new swoole_websocket_server("0.0.0.0", 9501);
 
 $server->on('open', function (swoole_websocket_server $server, $request) {//建立链接
     
     // $type = "login";
     // $msg = $request->fd."已登录";
-    // $reply = json_decode(array("type"=>$type,"msg"=>$msg));
+    // echo $request->fd."login success\n";
+    // $reply = json_encode(array("type"=>$type,"msg"=>$msg));
     // $server->push($request->fd,$reply);
+    //获取用户列表
+    //getListUser($server,$request->fd);
+    //var_dump($userlist,$request->fd);
+    
 });
 
 $server->on('message', function (swoole_websocket_server $server, $frame){//接受消息
+    //var_dump($redis);
     if($fd){
         $fd = ($fd>=$frame->fd)?$fd:$frame->fd;
     }else{
         $fd = $frame->fd;
     }
     $data = json_decode($frame->data);
-
+    //var_dump($data);
+    //登陆
     if($data->type == "login"){
-        $replyTtype = "login";
-        $msg = '用户'.$fd."登录\n";
-        $res = array("replyTtype"=>$replyTtype,"msg"=>$msg); 
-        $jsonRes = json_encode($res);
-        addUser($frame->fd);
+        // $type = "login";
+        // $msg = '用户'.$fd."登录\n";
+        echo "login success\n";
+        // $res = array("type"=>$type,"msg"=>$msg); 
+        // $jsonRes = json_encode($res);
+        echo "$fd\n";
+        addUser($frame->fd);//redis增加用户
+        // echo "user-$fd\n";
+        msg($server,"login","user-$fd");//通知上线消息
+        //获取用户列表
+        getListUser($server,$fd);
     }elseif($data->type == "message"){
 
     }
 
-    //var_dump($data->type);
-    for ($i=1; $i <=$fd ; $i++) {
-        $server->push($i,$jsonRes);
-    }
+
 });
 
 //$server->send();
 $server->on('close', function ($ser, $fd){
-    delUser($fd);
+    delUser($fd);//redis删除用户
+    getListUser($ser,$fd);//重新获取用户列表
+    msg($ser,"close","user-$fd");//通知下线消息
     echo "client {$fd} closed\n";
 });
 $server->start();
@@ -57,10 +70,11 @@ $server->start();
  * @date    2016-06-16
  */
 function addUser($fd){
-    global $redis;
+    // global $redis;
+    $redis = new swoole_redis;
         $redis->connect('127.0.0.1', 6379, function (swoole_redis $redis, $result) use($fd){
+            var_dump($result);
             $redis->set("user-".$fd,"fd-".$fd, function($redis,$result){ 
-                var_dump($result);
             });
         });
 }
@@ -71,10 +85,11 @@ function addUser($fd){
  * @return [type] [description]
  */
 function delUser($fd){
-    global $redis;
+    // global $redis;
+    // var_dump($redis);
+        $redis = new swoole_redis;
         $redis->connect('127.0.0.1', 6379, function (swoole_redis $redis, $result) use($fd){
             $redis->del("user-".$fd, function($redis,$result){ 
-                var_dump($result);
             });
         });
 }
@@ -84,6 +99,33 @@ function delUser($fd){
  * @date          2016-06-16
  * @return [type] [description]
  */
-function getListUser(){
+function getListUser($server,$fd){
+    $redis = new swoole_redis;
+    $redis->connect('127.0.0.1', 6379, function (swoole_redis $redis, $result) use($server,$fd){
+        $redis->keys("user-*", function(swoole_redis $client,$result) use($server,$fd){
+            for($i=0;$i<count($result);$i++){
+                $cliendId = str_replace("user-" , "", $result[$i]);//获取在线用户，发送消息
+                $server->push($cliendId,json_encode(array("type"=>"userlist","msg"=>$result)));
+            }
+            
+        });
+    });
+}
 
+function msg($server,$type,$user){
+    echo "type=$type\n";
+    $redis = new swoole_redis;
+    $redis->connect('127.0.0.1', 6379, function (swoole_redis $redis, $result) use($server,$type,$user){
+        $redis->keys("user-*", function(swoole_redis $client,$result) use($server,$type,$user){
+            for($i=0;$i<count($result);$i++){
+                $cliendId = str_replace("user-" , "", $result[$i]);//获取在线用户，发送消息
+                if($type == "login"){
+                    $data = array("type"=>"login","msg"=>$user."已上线");
+                }else if($type=="close"){
+                    $data = array("type"=>"close","msg"=>$user."已下线");
+                }
+                $server->push($cliendId,json_encode($data));
+            }
+        });
+    });   
 }
